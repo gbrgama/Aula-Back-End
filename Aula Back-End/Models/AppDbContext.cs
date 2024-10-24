778d189881fe465fcfc23ad057c65d3f8d9e1c72
Gabriel <gabrielgama336@gmail.com>
2024-10-24 12:48:59 -0300

    Configuração do banco de dados e da tabela Veiculo.

---
+++ Aula Back-End/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Aula_Back_End.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+
+    }
+}
